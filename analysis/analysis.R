4a5cb390e322112b95716837cc54823778818c74
ChristopherGustafson <christopher.gustafson@outlook.com>
2021-09-18 13:59:11 +0200

    regression for expected model equation

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,10 +1,35 @@
+# Imports
 library(data.table);
 library(matrixStats);
+
+# Read data files
 js_data <- read.csv(file = "data-generation/javascript-data.csv");
-data <- data.frame(ID = js_data[, 1],
+# Find list size to execution time median rows.
+time_func <- function(n, k1, k2) {
+  n * log10(n) * k1 + n * k2
+};
+js_data_refined <- data.frame(ID = js_data[, 1],
 Medians = rowMedians(as.matrix(js_data[, -1])));
-jpeg(file = "out/plot.jpeg")
-plot(data[, 1], data[, 2],
+x <- js_data_refined[, 1];
+y <- js_data_refined[, 2];
+jpeg(file = "out/js-plot.jpeg")
+plot(x, y,
 xlab = "User list size", ylab = "Execution time (s)");
+curve(time_func(x, 1, 1), add = TRUE);
+fit <- nls(y~time_func(x, k1, k2), start = list(k1 = 1, k2 = 2))
 dev.off();
-print(data);
+print(fit)
+
+
+wasm_data <- read.csv(file = "data-generation/webasm-data.csv");
+wasm_data_refined <- data.frame(ID = wasm_data[, 1],
+Medians = rowMedians(as.matrix(wasm_data[, -1])));
+x <- wasm_data_refined[, 1];
+y <- wasm_data_refined[, 2];
+jpeg(file = "out/wasm-plot.jpeg")
+plot(x, y,
+xlab = "User list size", ylab = "Execution time (s)");
+curve(time_func(x, 1, 1), add = TRUE);
+fit <- nls(y~time_func(x, k1, k2), start = list(k1 = 1, k2 = 1))
+dev.off();
+print(fit)
